52cae1d3cc690d99cb7c5bafcc435fd9dd060e6a
ayzekinus <aytekinn@hotmail.com>
2026-03-12 16:44:50 +0100

    delete

--- src/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const SUPABASE_URL = "https://tamdwyoulwtufhjhvdnn.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhbWR3eW91bHd0dWZoamh2ZG5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzMTE1ODQsImV4cCI6MjA4ODg4NzU4NH0.stIDyNh8rBRJiZa8kVOoy6lh3dqOLKzghkwSoqSVbc0";
-
-export const isConfigured = true;
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
